4cb17e1c50abb90574b3b3d26900b657eebdb13e
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2020-10-02 09:56:21 +0000

    da continuare

---
+++ client/src/app/foo-component/foo-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-foo-component',
+  templateUrl: './foo-component.component.html',
+  styleUrls: ['./foo-component.component.css']
+})
+export class FooComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
